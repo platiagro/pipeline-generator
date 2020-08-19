6bc69bd5d81c7434e9fccfd02c918473be5730c7
Alende Geraldo <meubebe21@hotmail.com>
2020-08-19 16:05:31 -0300

    create logger.log file and save it in the minio

---
+++ pipelines/logger.py
@@ -0,0 +1,55 @@
+import os
+from os import getenv
+import sys
+
+from minio import Minio
+from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
+                         BucketAlreadyExists)
+
+client = Minio(
+    endpoint=getenv('MINIO_ENDPOINT', 'minio-service.kubeflow:9000'),
+    access_key=getenv('MINIO_ACCESS_KEY', 'minio'),
+    secret_key=getenv("MINIO_SECRET_KEY", 'minio123'),
+    region=getenv('MINIO_REGION_NAME', 'us-east-1'),
+    secure=False,
+               )
+
+
+def create_seldon_logger(experiment_id, data):
+    print(f'dataset log {data}')
+    try:
+        objects = client.list_objects_v2('anonymous', recursive=True, prefix=f'components/{experiment_id}/')
+
+        if objects:
+            response = client.get_object('anonymous', f'components/{experiment_id}/seldon.log')
+            filedir = os.path.dirname(os.path.realpath('__file__'))
+            filename = os.path.join(filedir, 'seldon.log')
+            created_file(data, response)
+            with open(filename, 'rb', buffering=0) as data:
+                st_size = sys.getsizeof(data)
+                client.put_object('anonymous', f'components/{experiment_id}/seldon.log', data, st_size)
+                os.remove('seldon.log')
+            response.close()
+            response.release_conn()
+        else:
+            created_file(data, None)
+            with open('seldon.log', 'rb', buffering=0) as data:
+                file_stat = os.stat('seldon.log')
+                client.put_object('anonymous', f'components/{experiment_id}/seldon.log', data, file_stat.st_size)
+                os.remove('seldon.log')
+    except BucketAlreadyOwnedByYou as err:
+        pass
+    except BucketAlreadyExists as er:
+        pass
+    except ResponseError as err:
+        raise
+
+
+def created_file(data, response):
+    f = open('seldon.log', 'w+')
+    f.write(data)
+    f.write('\n')
+    if response:
+        my_response = response.read().decode("utf-8")
+        f.write(my_response)
+    f.closed
