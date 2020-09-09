bf201c653ddb2444f0191f2820b20779646de034
Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>
2020-09-09 10:28:13 -0300

    Correcting pipeline client endpoint

--- pipelines/utils.py
+++ pipelines/utils.py
@@ -23,7 +23,7 @@ def init_pipeline_client():
     Returns:
         An instance of kfp client.
     """
-    return Client(getenv("KF_PIPELINES_ENDPOINT", '10.50.11.60:31380/pipeline'), namespace="deployments")
+    return Client(getenv("KF_PIPELINES_ENDPOINT", '0.0.0.0:31380/pipeline'), namespace="deployments")
 
 
 def load_kube_config():
