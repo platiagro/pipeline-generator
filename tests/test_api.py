13d0393b225dceae8ba2271659301cb2f223df65
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-10-29 10:47:29 -0300

    Bumps to v0.2.0
    Updates:
    - __init__.py
    - Github actions (docker tag)
    - Docs (Swagger UI)
    - README
    - Tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,6 +10,6 @@ class TestApi(TestCase):
         with app.test_client() as c:
             rv = c.get("/")
             result = rv.get_data(as_text=True)
-            expected = "{\"message\":\"PlatIAgro Pipelines v0.1.0\"}\n"
+            expected = "{\"message\":\"PlatIAgro Pipelines v0.2.0\"}\n"
             self.assertEqual(result, expected)
             self.assertEqual(rv.status_code, 200)
