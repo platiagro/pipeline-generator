cf9754e1f67f091ebe22378866d7789cc18754c8
Miguel Ferraz <miguel.f.ferraz.27@gmail.com>
2020-11-05 14:16:09 -0300

    Add create_training test

--- tests/test_trainings.py
+++ tests/test_trainings.py
@@ -5,17 +5,108 @@ from pipelines.api.main import app
 from pipelines.utils import uuid_alpha
 
 COMPONENT_ID = str(uuid_alpha())
-EXPERIMENT_ID = str*(uuid_alpha())
+EXPERIMENT_ID = str(uuid_alpha())
 OPERATOR_ID = str(uuid_alpha())
+OPERATOR_ID_2 = str(uuid_alpha())
 TRAINING_ID = str(uuid_alpha())
 NOTEBOOK_PATH = f"minio://anonymous/components/{COMPONENT_ID}/Training.ipynb"
+IMAGE = "platiagro/platiagro-notebook-image:0.2.0"
 
 
 class TestTrainings(TestCase):
 
     def test_put_training(self):
         with app.test_client() as c:
-            rv = c.put(f"/trainings/{TRAINING_ID}", json={{
+
+            rv = c.put(f"/trainings/{TRAINING_ID}", json={
+                    "experimentId": EXPERIMENT_ID
+                }
+            )
+            result = rv.get_json()
+            expected = {"message": "Invalid request body, missing the parameter: 'operators'"}
+            self.assertDictEqual(expected, result)
+            self.assertEqual(rv.status_code, 400)
+
+            rv = c.put(f"/trainings/{TRAINING_ID}", json={
+                    "experimentId": EXPERIMENT_ID,
+                    "operators": [],
+                }
+            )
+            result = rv.get_json()
+            expected = {"message": "Necessary at least one operator"}
+            self.assertDictEqual(expected, result)
+            self.assertEqual(rv.status_code, 400)
+
+            rv = c.put(f"/trainings/{TRAINING_ID}", json={
+                    "experimentId": EXPERIMENT_ID,
+                    "operators": [{
+                        "operatorId": OPERATOR_ID
+                    }],
+                }
+            )
+            result = rv.get_json()
+            expected = {"message": "Invalid operator in request."}
+            self.assertDictEqual(expected, result)
+            self.assertEqual(rv.status_code, 400)
+
+            rv = c.put(f"/trainings/{TRAINING_ID}", json={
+                    "experimentId": EXPERIMENT_ID,
+                    "operators": [
+                        {
+                            "operatorId": OPERATOR_ID,
+                            "notebookPath": NOTEBOOK_PATH,
+                            "commands": [
+                                "cmd"
+                            ],
+                            "arguments": [],
+                            "dependencies": [OPERATOR_ID_2],
+                            "image": IMAGE,
+                            "parameters": [
+                                {
+                                    "name": "foo"
+                                }
+                            ]
+                        }
+                    ],
+                }
+            )
+            result = rv.get_json()
+            expected = {"message": "Invalid parameter."}
+            self.assertDictEqual(expected, result)
+            self.assertEqual(rv.status_code, 400)
+
+
+            # cyclical pipeline
+            rv = c.put(f"/trainings/{TRAINING_ID}", json={
+                    "experimentId": EXPERIMENT_ID,
+                    "operators": [
+                        {
+                            "operatorId": OPERATOR_ID,
+                            "notebookPath": NOTEBOOK_PATH,
+                            "commands": [
+                                "cmd"
+                            ],
+                            "arguments": [],
+                            "dependencies": [OPERATOR_ID_2],
+                            "image": IMAGE
+                        },
+                        {
+                            "operatorId": OPERATOR_ID_2,
+                            "notebookPath": NOTEBOOK_PATH,
+                            "commands": [],
+                            "arguments": [],
+                            "dependencies": [OPERATOR_ID],
+                            "image": IMAGE
+                        }
+                    ]
+                }
+            )
+            result = rv.get_json()
+            expected = {"message": "The given pipeline has cycles."}
+            self.assertDictEqual(expected, result)
+            self.assertEqual(rv.status_code, 400)
+
+            rv = c.put(f"/trainings/{TRAINING_ID}", json={
                     "experimentId": EXPERIMENT_ID,
                     "operators": [
                         {
@@ -24,17 +115,18 @@ class TestTrainings(TestCase):
                         "commands": [
                             "cmd"
                         ],
+                        "arguments": [],
                         "image": "platiagro/platiagro-notebook-image:0.2.0"
                         }
                     ]
-                }}
+                }
             )
             result = rv.get_json()
             expected = {"message": "Pipeline running."}
 
             # uuid is machine-generated
             # we assert they exist, but we don't assert their values
-            self.assertIn("uuid", result)
-            del result["uuid"]
+            self.assertIn("runId", result)
+            del result["runId"]
 
             self.assertDictEqual(expected, result)
