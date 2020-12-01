b66ae4cfddee70f5f373d5f35cd16419f2410b03
Ricardo Shiguemi Hiramatsu <38075762+shiguemi32@users.noreply.github.com>
2020-12-01 08:58:16 -0300

    Get complete dataset (#108)
    * Get complete dataset
    
    * Fix dataset unit test
    
    Co-authored-by: Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>

--- pipelines/api/datasets.py
+++ pipelines/api/datasets.py
@@ -1,24 +1,35 @@
 # -*- coding: utf-8 -*-
+from flask import make_response, request
 from flask_smorest import Blueprint
 
-from pipelines.controllers.datasets import get_dataset_pagination
+from pipelines.controllers.datasets import get_dataset_name, get_dataset_pagination
 
 bp = Blueprint("datasets", __name__)
 
 
 @bp.route("", methods=["GET"])
-@bp.paginate()
 def handle_list_datasets_by_run_id(project_id,
                                    experiment_id,
                                    run_id,
-                                   operator_id,
-                                   pagination_parameters):
+                                   operator_id):
     """Handles GET requests to /."""
-    pagination_parameters.item_count = 0
-    datasets = get_dataset_pagination(experiment_id=experiment_id,
+    accept = request.headers.get('Accept')
+    application_csv = False
+    if accept and 'application/csv' in accept:
+        application_csv = True
+    page = int(request.args.get('page', default=1))
+    page_size = int(request.args.get('page_size', default=10))
+    dataset_name = get_dataset_name(experiment_id=experiment_id, operator_id=operator_id,)
+    datasets = get_dataset_pagination(application_csv=application_csv,
+                                      name=dataset_name,
                                       operator_id=operator_id,
-                                      page=pagination_parameters.page,
-                                      page_size=pagination_parameters.page_size,
+                                      page=page,
+                                      page_size=page_size,
                                       run_id=run_id)
 
+    if application_csv:
+        response = make_response(datasets)
+        response.headers["Content-Disposition"] = f"attachment; filename={dataset_name}"
+        response.headers["Content-type"] = "text/csv"
+        return response
     return datasets
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -12,23 +12,21 @@ from pipelines.object_storage import BUCKET_NAME, MINIO_CLIENT
 from pipelines.utils import uuid_alpha
 
 PROJECT_ID = str(uuid_alpha())
-EXPERIMENT_ID = str(uuid_alpha())
 TASK_ID = str(uuid_alpha())
-OPERATOR_ID = str(uuid_alpha())
 RUN_ID = str(uuid_alpha())
+EXP_ID_1 = str(uuid_alpha())
+OP_ID_1_1 = str(uuid_alpha())
+OP_ID_1_2 = str(uuid_alpha())
+EXP_ID_2 = str(uuid_alpha())
+OP_ID_2_1 = str(uuid_alpha())
+OP_ID_2_2 = str(uuid_alpha())
+EXP_ID_3 = str(uuid_alpha())
+OP_ID_3_1 = str(uuid_alpha())
 NAME = "foo"
-DESCRIPTION = "long foo"
 DATASET = "mock.csv"
-TARGET = "col4"
-POSITION = 0
-PARAMETERS = {"dataset": DATASET}
-COMMANDS = ["CMD"]
-COMMANDS_JSON = dumps(COMMANDS)
-ARGUMENTS = ["ARG"]
-ARGUMENTS_JSON = dumps(ARGUMENTS)
+DATASET_RUN_ID_NONE = "teste.csv"
 IMAGE = "platiagro/platiagro-notebook-image-test:0.2.0"
-TAGS = ["PREDICTOR"]
-TAGS_JSON = dumps(TAGS)
+PARAMETERS = {"dataset": DATASET}
 PARAMETERS_JSON = dumps(PARAMETERS)
 EXPERIMENT_NOTEBOOK_PATH = f"minio://{BUCKET_NAME}/tasks/{TASK_ID}/Experiment.ipynb"
 DEPLOYMENT_NOTEBOOK_PATH = f"minio://{BUCKET_NAME}/tasks/{TASK_ID}/Deployment.ipynb"
@@ -41,6 +39,12 @@ class TestDatasets(TestCase):
         self.maxDiff = None
         conn = engine.connect()
         text = (
+            f"INSERT INTO tasks (uuid, name, description, image, commands, arguments, tags, experiment_notebook_path, deployment_notebook_path, is_default, created_at, updated_at) "
+            f"VALUES ('{TASK_ID}', '{NAME}', 'long foo', '{IMAGE}', '{dumps(['CMD'])}', '{dumps(['ARG'])}', '{dumps(['PREDICTOR'])}', '{EXPERIMENT_NOTEBOOK_PATH}', '{DEPLOYMENT_NOTEBOOK_PATH}', 0, '{CREATED_AT}', '{UPDATED_AT}')"
+        )
+        conn.execute(text)
+
+        text = (
             f"INSERT INTO projects (uuid, name, created_at, updated_at) "
             f"VALUES ('{PROJECT_ID}', '{NAME}', '{CREATED_AT}', '{UPDATED_AT}')"
         )
@@ -48,19 +52,44 @@ class TestDatasets(TestCase):
 
         text = (
             f"INSERT INTO experiments (uuid, name, project_id, position, is_active, created_at, updated_at) "
-            f"VALUES ('{EXPERIMENT_ID}', '{NAME}', '{PROJECT_ID}', '{POSITION}', 1, '{CREATED_AT}', '{UPDATED_AT}')"
+            f"VALUES ('{EXP_ID_1}', '{NAME}', '{PROJECT_ID}', 0, 1, '{CREATED_AT}', '{UPDATED_AT}')"
         )
         conn.execute(text)
-
-        text = (
-            f"INSERT INTO tasks (uuid, name, description, image, commands, arguments, tags, experiment_notebook_path, deployment_notebook_path, is_default, created_at, updated_at) "
-            f"VALUES ('{TASK_ID}', '{NAME}', '{DESCRIPTION}', '{IMAGE}', '{COMMANDS_JSON}', '{ARGUMENTS_JSON}', '{TAGS_JSON}', '{EXPERIMENT_NOTEBOOK_PATH}', '{DEPLOYMENT_NOTEBOOK_PATH}', 0, '{CREATED_AT}', '{UPDATED_AT}')"
-        )
-        conn.execute(text)
-
         text = (
             f"INSERT INTO operators (uuid, experiment_id, task_id, parameters, created_at, updated_at) "
-            f"VALUES ('{OPERATOR_ID}', '{EXPERIMENT_ID}', '{TASK_ID}', '{PARAMETERS_JSON}', '{CREATED_AT}', '{UPDATED_AT}')"
+            f"VALUES ('{OP_ID_1_1}', '{EXP_ID_1}', '{TASK_ID}', '{PARAMETERS_JSON}', '{CREATED_AT}', '{UPDATED_AT}')"
+        )
+        conn.execute(text)
+        text = (
+            f"INSERT INTO operators (uuid, experiment_id, task_id, parameters, created_at, updated_at) "
+            f"VALUES ('{OP_ID_1_2}', '{EXP_ID_1}', '{TASK_ID}', '{dumps({})}', '{CREATED_AT}', '{UPDATED_AT}')"
+        )
+        conn.execute(text)
+
+        text = (
+            f"INSERT INTO experiments (uuid, name, project_id, position, is_active, created_at, updated_at) "
+            f"VALUES ('{EXP_ID_2}', '{NAME}', '{PROJECT_ID}', 1, 1, '{CREATED_AT}', '{UPDATED_AT}')"
+        )
+        conn.execute(text)
+        text = (
+            f"INSERT INTO operators (uuid, experiment_id, task_id, parameters, created_at, updated_at) "
+            f"VALUES ('{OP_ID_2_1}', '{EXP_ID_2}', '{TASK_ID}', '{dumps({})}', '{CREATED_AT}', '{UPDATED_AT}')"
+        )
+        conn.execute(text)
+        text = (
+            f"INSERT INTO operators (uuid, experiment_id, task_id, parameters, created_at, updated_at) "
+            f"VALUES ('{OP_ID_2_2}', '{EXP_ID_2}', '{TASK_ID}', '{dumps({})}', '{CREATED_AT}', '{UPDATED_AT}')"
+        )
+        conn.execute(text)
+
+        text = (
+            f"INSERT INTO experiments (uuid, name, project_id, position, is_active, created_at, updated_at) "
+            f"VALUES ('{EXP_ID_3}', '{NAME}', '{PROJECT_ID}', 2, 1, '{CREATED_AT}', '{UPDATED_AT}')"
+        )
+        conn.execute(text)
+        text = (
+            f"INSERT INTO operators (uuid, experiment_id, task_id, parameters, created_at, updated_at) "
+            f"VALUES ('{OP_ID_3_1}', '{EXP_ID_3}', '{TASK_ID}', '{dumps({'dataset': DATASET_RUN_ID_NONE})}', '{CREATED_AT}', '{UPDATED_AT}')"
         )
         conn.execute(text)
         conn.close()
@@ -98,23 +127,31 @@ class TestDatasets(TestCase):
         )
         MINIO_CLIENT.copy_object(
             bucket_name=BUCKET_NAME,
-            object_name=f"datasets/{DATASET}/runs/{RUN_ID}/operators/{OPERATOR_ID}/{DATASET}/{DATASET}",
+            object_name=f"datasets/{DATASET}/runs/{RUN_ID}/operators/{OP_ID_1_1}/{DATASET}/{DATASET}",
             object_source=f"/{BUCKET_NAME}/datasets/{DATASET}/{DATASET}",
         )
         MINIO_CLIENT.copy_object(
             bucket_name=BUCKET_NAME,
-            object_name=f"datasets/{DATASET}/runs/{RUN_ID}/operators/{OPERATOR_ID}/{DATASET}/{DATASET}.metadata",
+            object_name=f"datasets/{DATASET}/runs/{RUN_ID}/operators/{OP_ID_1_1}/{DATASET}/{DATASET}.metadata",
             object_source=f"/{BUCKET_NAME}/datasets/{DATASET}/{DATASET}.metadata",
+        )
+
+        buffer = BytesIO(dumps({}).encode())
+        MINIO_CLIENT.put_object(
+            bucket_name=BUCKET_NAME,
+            object_name=f"datasets/{DATASET_RUN_ID_NONE}/{DATASET_RUN_ID_NONE}.metadata",
+            data=buffer,
+            length=buffer.getbuffer().nbytes,
         )
 
     def tearDown(self):
         MINIO_CLIENT.remove_object(
             bucket_name=BUCKET_NAME,
-            object_name=f"datasets/{DATASET}/runs/{RUN_ID}/operators/{OPERATOR_ID}/{DATASET}/{DATASET}.metadata",
+            object_name=f"datasets/{DATASET}/runs/{RUN_ID}/operators/{OP_ID_1_1}/{DATASET}/{DATASET}.metadata",
         )
         MINIO_CLIENT.remove_object(
             bucket_name=BUCKET_NAME,
-            object_name=f"datasets/{DATASET}/runs/{RUN_ID}/operators/{OPERATOR_ID}/{DATASET}/{DATASET}",
+            object_name=f"datasets/{DATASET}/runs/{RUN_ID}/operators/{OP_ID_1_1}/{DATASET}/{DATASET}",
         )
         MINIO_CLIENT.remove_object(
             bucket_name=BUCKET_NAME,
@@ -124,9 +161,13 @@ class TestDatasets(TestCase):
             bucket_name=BUCKET_NAME,
             object_name=f"datasets/{DATASET}/{DATASET}",
         )
+        MINIO_CLIENT.remove_object(
+            bucket_name=BUCKET_NAME,
+            object_name=f"datasets/{DATASET_RUN_ID_NONE}/{DATASET_RUN_ID_NONE}.metadata",
+        )
 
         conn = engine.connect()
-        text = f"DELETE FROM operators WHERE experiment_id = '{EXPERIMENT_ID}'"
+        text = f"DELETE FROM operators WHERE experiment_id in ('{EXP_ID_1}', '{EXP_ID_2}', '{EXP_ID_3}')"
         conn.execute(text)
 
         text = f"DELETE FROM tasks WHERE uuid = '{TASK_ID}'"
@@ -141,19 +182,27 @@ class TestDatasets(TestCase):
 
     def test_get_dataset(self):
         with app.test_client() as c:
-            rv = c.get(f"/projects/1/experiments/unk/runs/{RUN_ID}/operators/{OPERATOR_ID}/datasets")
+            rv = c.get(f"/projects/1/experiments/unk/runs/{RUN_ID}/operators/{OP_ID_1_1}/datasets")
             result = rv.get_json()
             expected = {"message": "The specified experiment does not exist"}
             self.assertDictEqual(expected, result)
             self.assertEqual(rv.status_code, 404)
 
-            rv = c.get(f"/projects/1/experiments/{EXPERIMENT_ID}/runs/{RUN_ID}/operators/unk/datasets")
+            rv = c.get(f"/projects/1/experiments/{EXP_ID_1}/runs/{RUN_ID}/operators/unk/datasets")
             result = rv.get_json()
             expected = {"message": "The specified operator does not exist"}
             self.assertDictEqual(expected, result)
             self.assertEqual(rv.status_code, 404)
 
-            rv = c.get(f"/projects/1/experiments/{EXPERIMENT_ID}/runs/{RUN_ID}/operators/{OPERATOR_ID}/datasets")
+            # operators without dataset paramater
+            rv = c.get(f"/projects/1/experiments/{EXP_ID_2}/runs/{RUN_ID}/operators/{OP_ID_2_2}/datasets")
+            self.assertEqual(rv.status_code, 404)
+
+            # dataset without run_id in metadata
+            rv = c.get(f"/projects/1/experiments/{EXP_ID_3}/runs/{RUN_ID}/operators/{OP_ID_3_1}/datasets")
+            self.assertEqual(rv.status_code, 404)
+
+            rv = c.get(f"/projects/1/experiments/{EXP_ID_1}/runs/{RUN_ID}/operators/{OP_ID_1_1}/datasets")
             result = rv.get_json()
             expected = {
                 "columns": ["col0", "col1", "col2", "col3", "col4", "col5"],
@@ -165,3 +214,40 @@ class TestDatasets(TestCase):
                 "total": 3
             }
             self.assertDictEqual(expected, result)
+
+            rv = c.get(f"/projects/1/experiments/{EXP_ID_1}/runs/{RUN_ID}/operators/{OP_ID_1_2}/datasets")
+            result = rv.get_json()
+            expected = {
+                "columns": ["col0", "col1", "col2", "col3", "col4", "col5"],
+                "data": [
+                    ["01/01/2000", 5.1, 3.5, 1.4, 0.2, "Iris-setosa"],
+                    ["01/01/2000", 5.1, 3.5, 1.4, 0.2, "Iris-setosa"],
+                    ["01/01/2000", 5.1, 3.5, 1.4, 0.2, "Iris-setosa"]
+                ],
+                "total": 3
+            }
+            self.assertDictEqual(expected, result)
+
+            rv = c.get(f"/projects/1/experiments/{EXP_ID_1}/runs/{RUN_ID}/operators/{OP_ID_1_1}/datasets?page_size=-1")
+            result = rv.get_json()
+            expected = {
+                "columns": ["col0", "col1", "col2", "col3", "col4", "col5"],
+                "data": [
+                    ["01/01/2000", 5.1, 3.5, 1.4, 0.2, "Iris-setosa"],
+                    ["01/01/2000", 5.1, 3.5, 1.4, 0.2, "Iris-setosa"],
+                    ["01/01/2000", 5.1, 3.5, 1.4, 0.2, "Iris-setosa"]
+                ],
+            }
+            self.assertDictEqual(expected, result)
+
+            rv = c.get(f"/projects/1/experiments/{EXP_ID_1}/runs/{RUN_ID}/operators/{OP_ID_1_1}/datasets",
+                       headers={'Accept': 'application/csv'})
+            result = rv.data
+            expected = b'col0,col1,col2,col3,col4,col5\n01/01/2000,5.1,3.5,1.4,0.2,Iris-setosa\n01/01/2000,5.1,3.5,1.4,0.2,Iris-setosa\n01/01/2000,5.1,3.5,1.4,0.2,Iris-setosa\n'
+            self.assertEquals(expected, result)
+
+            rv = c.get(f"/projects/1/experiments/{EXP_ID_1}/runs/{RUN_ID}/operators/{OP_ID_1_1}/datasets?page_size=-1",
+                       headers={'Accept': 'application/csv'})
+            result = rv.data
+            expected = b'col0,col1,col2,col3,col4,col5\n01/01/2000,5.1,3.5,1.4,0.2,Iris-setosa\n01/01/2000,5.1,3.5,1.4,0.2,Iris-setosa\n01/01/2000,5.1,3.5,1.4,0.2,Iris-setosa\n'
+            self.assertEquals(expected, result)
